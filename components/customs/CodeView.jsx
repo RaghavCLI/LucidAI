81528527c7045b30ffc2e5b7afd9c39ca6851882
viper-raghav <upadhyayraghav17@gmail.com>
2025-08-24 17:16:32 +0530

    code view

--- components/customs/CodeView.jsx
+++ components/customs/CodeView.jsx
@@ -1,9 +1,65 @@
+"use client";
 import React from "react";
+import {
+  SandpackProvider,
+  SandpackLayout,
+  SandpackCodeEditor,
+  SandpackPreview,
+  SandpackFileExplorer,
+} from "@codesandbox/sandpack-react";
+import { useState } from "react";
+import Lookup from "@/app/data/Lookup";
 
 function CodeView() {
+  const [activeTab, setActiveTab] = useState("code");
+  const [files, setFiles] = useState(Lookup?.DEFAULT_FILE);
   return (
     <div>
-      <h1>Code View</h1>
+      <div className="bg-[#181818 w-full p-2 border">
+        <div className="flex items-center flex-wrap shrink-0 bg-black p-1 w-[140px] gap-3">
+          <h2
+            onClick={() => setActiveTab("code")}
+            className={`text-sm cursor-pointer ${activeTab == "code" && "text-blue-500 bg-opacity-25 p-1 px-2 rounded-full"}`}
+          >
+            Code
+          </h2>
+          <h2
+            onClick={() => setActiveTab("preview")}
+            className={`text-sm cursor-pointer ${activeTab == "preview" && "text-blue-500 bg-opacity-25 p-1 px-2 rounded-full"}`}
+          >
+            Preview
+          </h2>
+        </div>
+      </div>
+      <SandpackProvider
+        files={files}
+        template="react"
+        theme={"dark"}
+        customSetup={{
+          dependencies: {
+            ...Lookup.DEPENDANCY,
+          },
+        }}
+        options={{
+          externalResources: ["https://cdn.tailwindcss.com"],
+        }}
+      >
+        <SandpackLayout>
+          {activeTab == "code" ? (
+            <>
+              <SandpackFileExplorer style={{ height: "90vh" }} />
+              <SandpackCodeEditor style={{ height: "90vh" }} />
+            </>
+          ) : (
+            <>
+              <SandpackPreview
+                style={{ height: "90vh" }}
+                showNavigator={true}
+              />
+            </>
+          )}
+        </SandpackLayout>
+      </SandpackProvider>
     </div>
   );
 }
